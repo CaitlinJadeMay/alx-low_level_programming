9fbb63a01c983de5c27cbb3ad113e2f924b4ed06
Caitlin <caitlinjadelingeveldt@gmail.com>
2023-04-21 11:33:18 -0700

    three

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,12 +7,16 @@
 int main(void)
 {
 	int n = 97;
+	int m = 65;
 
 	while (n <= 122)
 	{
 		putchar(n);
-		n++;
 	}
-	putchar('\n');
-	return (0);
+	while (m <= 90)
+	{
+		putchar(m);
+	}
+	putchar('\n')
+		return (0);
 }
