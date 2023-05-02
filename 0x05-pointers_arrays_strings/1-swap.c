4b09c9b28888bacb652097bc928409f7adc37c0a
Caitlin <caitlinjadelingeveldt@gmail.com>
2023-05-02 13:41:20 -0700

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *1-swap: Write a function that swaps the values of two integers
+ *swap_int - Write a function that swaps the values of two integers
  *@a: swap integer with b
  *@b: swap integer with a
  *Return: (0) success
