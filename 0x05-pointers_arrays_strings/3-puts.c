866ef2b719a23e95e73b19110b999d5a34d0d523
Caitlin <caitlinjadelingeveldt@gmail.com>
2023-05-02 14:13:05 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_puts - Write a function that prints a string,
+ *followed by a new line, to stdout
+ *@str: pointer to the string
+ *Return: String followed by a new line
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		putchar(*str++);
+	}
+		putchar('\n');
+}
