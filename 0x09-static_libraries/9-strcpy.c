06b6e89be98879811289873fea1898f534050672
Caitlin <caitlinjadelingeveldt@gmail.com>
2023-05-17 11:17:56 -0700

    limby

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -18,7 +18,7 @@ char *_strcpy(char *dest, char *src)
 	{
 		h++;
 	}
-	for ( ; ca < h ; ca +)
+	for ( ; ca < h ; ca++ )
 	{
 		dest[ca] = src[ca];
 	}
--- 0x09-static_libraries/README.md
+++ 0x09-static_libraries/README.md
@@ -1,1 +1,2 @@
+#!/bin/bash
 This README.md is for 0x09. C - Static libraries
