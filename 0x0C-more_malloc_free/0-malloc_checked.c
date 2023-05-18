51ffbc8ea9fc1288fb3c6c4932fc6e6d1a66cd8d
Caitlin <caitlinjadelingeveldt@gmail.com>
2023-05-18 04:33:58 -0700

    malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,13 +2,13 @@
 #include "main.h"
 
  /**
- * malloc_checked - allocates memory using malloc
+ * *malloc_checked - allocates memory using malloc
  * @b: number of bytes to allocate
  *
  * Return: a pointer to the allocated memory
  */
 
-void *malloc_checked(unsigned int b)
+void *malloc_checked(unsigned int b);
 {
 	void *cait;
 
