ac147dd01293a06bd8b1b5aef354e300fadad743
CaitlinJadeMay <caitlinjadelingeveldt@gmail.com>
2023-07-28 12:11:26 -0700

    numbers

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int add(int c, int s)
+{
+return (c + s);
+}
+int sub(int c, int s)
+{
+return (c - s);
+}
+
+int mul(int c, int s)
+{
+return (c * s);
+}
+
+int div(int c, int s)
+{
+return (c / s);
+}
+
+int mod(int c, int s)
+{
+return (c % s);
+}
